5e1a095a88eee92a1f8eaffbf4305acfbdcb2b3d
randa-magdy <48019673+randa-magdy@users.noreply.github.com>
2019-12-03 00:38:56 +0300

    Updates

---
+++ precache-manifest.bdc9974e6e46f6abe17dee22c84de410.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2160acf1f9a6127dd4670e42b61b56c",
+    "url": "/wokiee/index.html"
+  },
+  {
+    "revision": "ad2f16e7b9263f68ebdd",
+    "url": "/wokiee/static/css/2.1c5c90a3.chunk.css"
+  },
+  {
+    "revision": "77bc9604a421a2da0454",
+    "url": "/wokiee/static/css/main.4bbf99ed.chunk.css"
+  },
+  {
+    "revision": "ad2f16e7b9263f68ebdd",
+    "url": "/wokiee/static/js/2.9c834f4a.chunk.js"
+  },
+  {
+    "revision": "77bc9604a421a2da0454",
+    "url": "/wokiee/static/js/main.34c225b0.chunk.js"
+  },
+  {
+    "revision": "80b71c67ae5074c483cf",
+    "url": "/wokiee/static/js/runtime~main.ed4fe2f4.js"
+  },
+  {
+    "revision": "238aa715d71107fb0709832d9ce9ad5c",
+    "url": "/wokiee/static/media/about.238aa715.png"
+  },
+  {
+    "revision": "9655b5471359d27ce7852a1b06d9611f",
+    "url": "/wokiee/static/media/b1.9655b547.jpg"
+  },
+  {
+    "revision": "97a589a803a868bc66789a3ad02362af",
+    "url": "/wokiee/static/media/b4.97a589a8.jpg"
+  },
+  {
+    "revision": "64fe20816b80505768835fd5b644cc14",
+    "url": "/wokiee/static/media/b5.64fe2081.jpg"
+  },
+  {
+    "revision": "8980d2fbd4dc714dba419d6f27f83cb3",
+    "url": "/wokiee/static/media/b6.8980d2fb.jpg"
+  },
+  {
+    "revision": "02914bd44b23c9bb0b3c188756d9fa13",
+    "url": "/wokiee/static/media/blog_11.02914bd4.jpg"
+  },
+  {
+    "revision": "fdce6e580eaffdb69a60d0299387f67e",
+    "url": "/wokiee/static/media/blog_12.fdce6e58.jpg"
+  },
+  {
+    "revision": "ff9902e1d8c92220cd8a4c3fad9ce60c",
+    "url": "/wokiee/static/media/blog_13.ff9902e1.jpg"
+  },
+  {
+    "revision": "fe9f9dfe7400d5d3b6fe678d2e9319d4",
+    "url": "/wokiee/static/media/follow1.fe9f9dfe.jpg"
+  },
+  {
+    "revision": "64a27996c98269cfae099bf3a0e5b879",
+    "url": "/wokiee/static/media/follow2.64a27996.jpg"
+  },
+  {
+    "revision": "c53ab182cc320d0a7d367d1d98fbf193",
+    "url": "/wokiee/static/media/follow3.c53ab182.jpg"
+  },
+  {
+    "revision": "f10c61adb9a7944f01ba29360bd128b6",
+    "url": "/wokiee/static/media/follow4.f10c61ad.jpg"
+  },
+  {
+    "revision": "d9d4ad909b458625241f6ebddcdeaf2b",
+    "url": "/wokiee/static/media/follow5.d9d4ad90.jpg"
+  },
+  {
+    "revision": "d8ddfde4aeeb17a901b9db3affe8b75b",
+    "url": "/wokiee/static/media/follow6.d8ddfde4.jpg"
+  },
+  {
+    "revision": "736acbd7d7f629c8b387cfd4a14717e4",
+    "url": "/wokiee/static/media/header-category-01.736acbd7.jpg"
+  },
+  {
+    "revision": "38f7a9e36adad028c563997218d47c39",
+    "url": "/wokiee/static/media/header-category-02.38f7a9e3.jpg"
+  },
+  {
+    "revision": "a898e16662c9e99bd33f6e4b8aa2f66f",
+    "url": "/wokiee/static/media/header-category-03.a898e166.jpg"
+  },
+  {
+    "revision": "a44d09cae2840600a1082b6e4276f306",
+    "url": "/wokiee/static/media/img1.a44d09ca.jpg"
+  },
+  {
+    "revision": "99f12415a3ceb83ff4fb57f09589d309",
+    "url": "/wokiee/static/media/img2.99f12415.jpg"
+  },
+  {
+    "revision": "565913e0220be095db13d4f85482d238",
+    "url": "/wokiee/static/media/img3.565913e0.jpg"
+  },
+  {
+    "revision": "784829a34d69d39284649ddd63c26074",
+    "url": "/wokiee/static/media/index-promo-img-01.784829a3.jpg"
+  },
+  {
+    "revision": "75c232eba6c8c2792ef5dfa5bca717dc",
+    "url": "/wokiee/static/media/index-promo-img-02.75c232eb.jpg"
+  },
+  {
+    "revision": "04c7c52b886d621e3a03dd733663c58c",
+    "url": "/wokiee/static/media/index-promo-img-03.04c7c52b.jpg"
+  },
+  {
+    "revision": "db6fd694b0bcf09a311a6894971a34c5",
+    "url": "/wokiee/static/media/index-promo-img-04.db6fd694.jpg"
+  },
+  {
+    "revision": "2825f5e6f61c5973747b7092ff5755dd",
+    "url": "/wokiee/static/media/index-promo-img-05.2825f5e6.jpg"
+  },
+  {
+    "revision": "a9fd3856b62461f2424f593792c1ea80",
+    "url": "/wokiee/static/media/index-promo-img-06.a9fd3856.jpg"
+  },
+  {
+    "revision": "587583bcc32c7ec6dadda86b09a9a341",
+    "url": "/wokiee/static/media/mastercard.587583bc.jpg"
+  },
+  {
+    "revision": "404544110c3e0021e4463d72d6ef94ce",
+    "url": "/wokiee/static/media/megamenu_banner_right.40454411.jpg"
+  },
+  {
+    "revision": "0f91bc8622a306f60025b86fe46b43ff",
+    "url": "/wokiee/static/media/paypal.0f91bc86.png"
+  },
+  {
+    "revision": "779c139cdf7dbc4beeb7818997b1279f",
+    "url": "/wokiee/static/media/services1.779c139c.png"
+  },
+  {
+    "revision": "ed66b19e15498eba212186f924a5a03a",
+    "url": "/wokiee/static/media/services2.ed66b19e.png"
+  },
+  {
+    "revision": "4ca5370e1d4f465b4ce56decddb5f078",
+    "url": "/wokiee/static/media/services3.4ca5370e.png"
+  },
+  {
+    "revision": "286d4a8df16f6f1c4d7da3830d7c4a80",
+    "url": "/wokiee/static/media/services4.286d4a8d.png"
+  },
+  {
+    "revision": "6316994006dd1df6ab7befc8c5322ce9",
+    "url": "/wokiee/static/media/services5.63169940.png"
+  },
+  {
+    "revision": "dc7d41c41bb00c26756b67fc30319540",
+    "url": "/wokiee/static/media/services6.dc7d41c4.png"
+  },
+  {
+    "revision": "ccf2d6ab9c07c6bf7e06a0d90f16074a",
+    "url": "/wokiee/static/media/slide-01.ccf2d6ab.jpg"
+  },
+  {
+    "revision": "6cff9a96d2ebc8e34ef4e7bc8d682381",
+    "url": "/wokiee/static/media/slide-02.6cff9a96.jpg"
+  },
+  {
+    "revision": "67408b2d726f6122b44a9e3cfe22de9e",
+    "url": "/wokiee/static/media/slide-03.67408b2d.jpg"
+  },
+  {
+    "revision": "544c5197f20e57b77ada6de5b490316b",
+    "url": "/wokiee/static/media/visa.544c5197.png"
+  }
+]);
